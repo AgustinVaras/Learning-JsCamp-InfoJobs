7fef81e3d0b80b02ad1d35dbce8b3b4730eecfe3
AgustinVaras <Agustin.Varas@outlook.com.ar>
2026-03-10 12:43:45 +0100

    feat: added handle functions for onFocus and onBlur events to select elements of SearchFilters.jsx

--- Clase 5/react-project/src/components/SearchFilters.jsx
+++ Clase 5/react-project/src/components/SearchFilters.jsx
@@ -1,10 +1,35 @@
+import { useState } from "react"; 
 import styles from "./SearchFilters.module.css";
 
 export function SearchFilters( ) {
+    const [helpText, setHelptext] = useState("");
     
+    const handleFocus = (event) => {
+        const filterName = event.target.name;
+        let helpMessage = "";
+        switch (filterName){
+            case "technology" : 
+                helpMessage = "Selecciona una tecnología para filtrar los resultados de búsqueda.";
+                break;
+            case "location" :
+                helpMessage = "Selecciona una ubicación para filtrar los resultados de búsqueda.";
+                break;
+            case "level" :
+                helpMessage = "Selecciona un nivel de experiencia para filtrar los resultados de búsqueda.";
+                break;   
+        }
+        event.target.classList.add(styles.isFocused);
+        setHelptext(helpMessage);
+    }
+
+    const handleBlur = (event) => {
+        event.target.classList.remove(styles.isFocused);
+        setHelptext("");
+    }
+
     return (
         <div className={styles.dropDownWrapper}>
-            <select id="technology-filter" name="technology" defaultValue="" >
+            <select onFocus={handleFocus} onBlur={handleBlur} id="technology-filter" name="technology" defaultValue="" >
                 <option value="" >Tecnología</option>
                 <option value="javaScript">JavaScript</option>
                 <option value="react">React</option>
@@ -17,7 +42,7 @@ export function SearchFilters( ) {
                 <option value="azure">Azure</option>
                 <option value="gcp">GCP</option>
             </select>
-            <select id="location-filter" name="location" defaultValue="">
+            <select onFocus={handleFocus} onBlur={handleBlur} id="location-filter" name="location" defaultValue="">
                 <option value="" >Ubicación</option>
                 <option value="remoto">Remoto</option>
                 <option value="cdmx">Ciudad de México</option>
@@ -25,12 +50,13 @@ export function SearchFilters( ) {
                 <option value="barcelona">Barcelona</option>
                 <option value="bsas">Buenos Aires</option>
             </select>
-            <select id="level-filter" name="level" defaultValue="">
+            <select onFocus={handleFocus} onBlur={handleBlur} id="level-filter" name="level" defaultValue="">
                 <option value="" >Nivel de Experiencia</option>
                 <option value="junior">Junior</option>
                 <option value="mid-level">Mid-Level</option>
                 <option value="senior">Senior</option>
             </select>
+            {helpText && <p className={styles.filtersHelp}>{helpText}</p>}
         </div>
     )
 }
