5db02fe358b82a58b549bd31c33a96d8935bebac
AgustinVaras <Agustin.Varas@outlook.com.ar>
2026-04-09 16:52:54 +0200

    style: fixed styling

--- Clase 6/react-project/src/components/NoResults.jsx
+++ Clase 6/react-project/src/components/NoResults.jsx
@@ -3,6 +3,7 @@ import styles from './NoResults.module.css';
 export function NoResults() {
   return (
     <div className={styles.no_results}>
+      <span>🔍</span>
       <h3>No se encontraron han encontrado empleos que coincidan con los criterios de búsqueda.</h3>
     </div>
   );
